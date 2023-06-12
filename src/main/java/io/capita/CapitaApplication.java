0b86ba6c4ce83e31f508d0649958eb8eba7a37d9
Ndeta <ndetainnocent3@gmail.com>
2023-06-12 18:28:48 +0300

    Initial commit

---
+++ src/main/java/io/capita/CapitaApplication.java
@@ -0,0 +1,13 @@
+package io.capita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapitaApplication.class, args);
+    }
+
+}
